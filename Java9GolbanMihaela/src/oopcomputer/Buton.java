4687ca591d840e4b2d2b0eb70bd61fbaaf3a7656
Michaela <Michaela@Michaela-PC>
2018-09-29 01:54:40 +0300

    calculator final

---
+++ Java9GolbanMihaela/src/oopcomputer/Buton.java
@@ -0,0 +1,22 @@
+/*
+ * butoane calculator.
+ */
+package oopcomputer;
+
+/**
+ *
+ * @author Michaela
+ */
+public class Buton {
+
+    private Character simbol;
+
+    public Buton(Character noulSimbol) {
+        simbol = noulSimbol;
+    }
+
+    public Character apasa() {
+        return simbol;
+    }
+
+}
---
+++ Java9GolbanMihaela/src/oopcomputer/Ecran.java
@@ -0,0 +1,22 @@
+/*
+ * Ecranul calculatorului
+ */
+package oopcomputer;
+
+/**
+ *
+ * @author Michaela
+ */
+public class Ecran {
+
+    private String afisaj = "0";
+
+    public String getAfisaj() {
+        return afisaj;
+    }
+
+    public void setAfisaj(String afisaj) {
+        this.afisaj = afisaj;
+    }
+
+}
