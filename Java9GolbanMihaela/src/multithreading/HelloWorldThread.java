9320f5e961d2d59e2a6d43b8dea7e14c8516f451
Michaela <Michaela@Michaela-PC>
2018-11-23 23:47:21 +0200

    restored multi-threading missing classes

---
+++ Java9GolbanMihaela/src/multithreading/HelloWorldThread.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multithreading;
+
+/**
+ *
+ * @author Michaela
+ */
+public class HelloWorldThread extends Thread{
+
+    @Override
+    public void run() {
+        System.out.println("Salut Lume!");
+    }
+    
+    
+    
+}
